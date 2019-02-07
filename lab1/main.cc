735ab951d4563661d1bc010cf60751bf89e9a9e9
bjohv276 <bjohv276@su01-105.ad.liu.se>
2019-02-07 16:40:39 +0100

    Adds four sensors and generatros one for each direction, N S E W

--- lab1/main.cc
+++ lab1/main.cc
@@ -11,6 +11,13 @@ int sc_main(int argc, char **argv) {
 
   Generator gen_north("North_traffic_generator");
   gen_north(N_o_sig);
+  Generator gen_south("South_traffic_generator");
+  gen_north(S_o_sig);
+  Generator gen_west("West_traffic_generator");
+  gen_north(W_o_sig);
+  Generator gen_east("East_traffic_generator");
+  gen_north(E_o_sig);
+
   /*
   gen_1(N_o_sig);
   Generator gen_2("South_traffic_generator");
@@ -23,6 +30,13 @@ int sc_main(int argc, char **argv) {
 
   Sensor sensor_north("North_traffic_Sensor");
   sensor_north(N_o_sig);
+  Sensor sensor_south("South_traffic_Sensor");
+  sensor_north(S_o_sig);
+  Sensor sensor_west("West_traffic_Sensor");
+  sensor_north(W_o_sig);
+  Sensor sensor_east("East_traffic_Sensor");
+  sensor_north(E_o_sig);
+
 
 
   sc_start(300, SC_SEC);
