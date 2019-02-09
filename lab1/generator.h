9a286767049166c783d67d1603607de2ff804055
bjohv276 <bjohv276@su03-107.ad.liu.se>
2019-02-09 16:50:42 +0100

    Print event for genereator.h

--- lab1/generator.h
+++ lab1/generator.h
@@ -6,10 +6,12 @@
 #include <fstream>
 
 using std::ifstream;
+int nb_car = 100;
 
 SC_MODULE(Generator) {
   sc_out<bool> o_p;
   int C_queue [100];
+  sc_event print_event;
 
   SC_HAS_PROCESS(Generator);
   Generator(sc_module_name name, char *datafile);
