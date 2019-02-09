0aad56568a594432f0c2167dc4236cbc235671ef
bjohv276 <bjohv276@su03-107.ad.liu.se>
2019-02-09 14:40:01 +0100

    Extends controller.h to reflext controller.cc

--- lab1/controller.h
+++ lab1/controller.h
@@ -4,6 +4,11 @@
 SC_MODULE(Controller) {
   sc_in<bool> N_i_p, S_i_p, E_i_p, W_i_p;
   sc_out<bool> N_o_p, S_o_p, E_o_p, W_o_p;
+
+  SC_HAS_PROCESS(Controller);
+  Controller(sc_module_name name);
+
+  void controller_method();
 }
 
 #endif
