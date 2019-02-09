e7b9f7e3e3a795e2c34d823bcde2ffcbed99d649
bjohv276 <bjohv276@su03-107.ad.liu.se>
2019-02-09 16:50:24 +0100

    Print event with function

--- lab1/generator.cc
+++ lab1/generator.cc
@@ -4,8 +4,6 @@
 using std::cout;
 using std::endl;
 
-int nb_car = 100;
-
 Generator::Generator(sc_module_name name, char *datafile)
   : sc_module(name)
 {
@@ -13,15 +11,16 @@ Generator::Generator(sc_module_name name, char *datafile)
 
   in = new ifstream(datafile); // Open the input file.
   assert(*in);                 // Check that everything is OK.
-  
+
   SC_THREAD(gen_thread);
   SC_METHOD(print_method);
-
+  sensitive << print_event;
   o_p.initialize(0);
 }
 
 void Generator::print_method() {
-  cout << sc_time_stamp() << ": Cars " << o_p << endl;
+  const char* module_name = sc_core::sc_get_current_process_b()->get_parent()->basename();
+  cout << sc_time_stamp() << ": Cars for " << module_name << ": " << o_p->read() << endl;
 
 }
 
@@ -34,9 +33,11 @@ void Generator::gen_thread() {
   int i = 0;
   for (;;) {
     wait(8, SC_SEC);
+    print_event.notify();
     *in >> C_queue[i];
-    if ( ++i < nb_car ) {
+    if ( i < nb_car ) {
       o_p->write(C_queue[i]);
+      i++;
     }
     else{
       i = 0;
