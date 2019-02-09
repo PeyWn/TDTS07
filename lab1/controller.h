fab0e0753de2e67ba5590e2cb7157fae2037f392
bjohv276 <bjohv276@su03-107.ad.liu.se>
2019-02-09 15:52:19 +0100

    Extends controller.h with the enum struct for controllerState

--- lab1/controller.h
+++ lab1/controller.h
@@ -1,6 +1,13 @@
 #ifndef CONTRLLER_H
 #define CONTRLLER_H
 
+#include <systemc.h>
+
+struct ControllerState{
+  enum Traffic {WEST_BOUND, EAST_BOUND, EAST_AND_WEST_BOUND,
+    SOUTH_BOUND, NORTH_BOUND, NORTH_AND_SOUTH_BOUND, NONE};
+};
+
 SC_MODULE(Controller) {
   sc_in<bool> N_i_p, S_i_p, E_i_p, W_i_p;
   sc_out<bool> N_o_p, S_o_p, E_o_p, W_o_p;
