f88a821b6a82220fbb55a5ee770140cb78eb1a2f
Cyril Barrelet <cyril@c-10-253-239-153.clients.eduroam.liu.se>
2019-02-09 15:46:30 +0100

    Reading files to generate cars

--- lab1/generator.cc
+++ lab1/generator.cc
@@ -1,25 +1,23 @@
 #include "generator.h"
+#include <cassert>
 
 using std::cout;
 using std::endl;
 
 int nb_car = 100;
 
-void gen_random_list(int *array, int n) {
-  srand(time(NULL));
-  for (int i = 0; i <= n; i++)
-    array[i] = rand()%2;
-}
-
-Generator::Generator(sc_module_name name)
+Generator::Generator(sc_module_name name, char *datafile)
   : sc_module(name)
 {
-  o_p.initialize(0);
-  gen_random_list(C_queue, nb_car);
+  assert(datafile != 0);       // An input file should be given.
 
+  in = new ifstream(datafile); // Open the input file.
+  assert(*in);                 // Check that everything is OK.
+  
   SC_THREAD(gen_thread);
   SC_METHOD(print_method);
-  sensitive << o_p;
+
+  o_p.initialize(0);
 }
 
 void Generator::print_method() {
@@ -27,15 +25,20 @@ void Generator::print_method() {
 
 }
 
+Generator::~Generator()
+{
+  delete in;
+}
+
 void Generator::gen_thread() {
   int i = 0;
   for (;;) {
     wait(8, SC_SEC);
+    *in >> C_queue[i];
     if ( ++i < nb_car ) {
       o_p->write(C_queue[i]);
     }
     else{
-      gen_random_list(C_queue, nb_car);
       i = 0;
       }
   }
