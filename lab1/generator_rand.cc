49cf014da8f0fafa1f12505e522fcd3385a99f7a
bjohv276 <bjohv276@su01-110.ad.liu.se>
2019-02-14 17:11:43 +0100

    adds rand.cc

---
+++ lab1/generator_rand.cc
@@ -0,0 +1,68 @@
+#include "generator.h"
+#include <cassert>
+
+using std::cout;
+using std::endl;
+
+Generator::Generator(sc_module_name name, char *datafile)
+  : sc_module(name)
+{
+  assert(datafile != 0);       // An input file should be given.
+
+  in = new ifstream(datafile); // Open the input file.
+  assert(*in);                 // Check that everything is OK.
+
+  i = 0;
+  toggle = true;
+  srand(time(NULL)); //Init rand
+
+  getline(*in, line);
+  nb_car = atoi(line.c_str());
+  getline(*in, line);
+  in->close();
+
+  SC_THREAD(event_creator);
+  SC_METHOD(gen_method);
+  sensitive << gen_event;
+  SC_METHOD(print_method);
+  dont_initialize();
+  sensitive << print_event;
+  o_p.initialize(0);
+}
+
+void Generator::print_method() {
+  const char* module_name = sc_core::sc_get_current_process_b()->get_parent()->basename();
+  cout << sc_time_stamp() << ": Cars for " << module_name << ": " << o_p->read() << endl;
+
+}
+
+Generator::~Generator()
+{
+  delete in;
+}
+
+void Generator::gen_method() {
+  if ( i < nb_car) {
+    car = rand()%14; //car <= 4 30% chance of generating a car
+    if (car <= 4) {
+      if(toggle) {
+        o_p->write(true);
+        toggle = !toggle;
+      }
+      else {
+        o_p->write(false);
+        toggle = !toggle;
+      }
+    }
+    i++;
+  }
+}
+
+void Generator::event_creator() {
+  for(;;) {
+    wait(2, SC_SEC);
+    gen_event.notify();
+    //wait(0, SC_SEC);
+    //print_event.notify();
+  }
+}
