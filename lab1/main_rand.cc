02690187481ed044303a39be63759eebd7e0b4de
peywn <bjorn.hvass@gmail.com>
2019-03-06 12:47:15 +0100

    Update rand main and make rand

---
+++ lab1/main_rand.cc
@@ -0,0 +1,45 @@
+#include <systemc.h>
+#include "generator.h"
+#include "controller.h"
+#include "sensor.h"
+
+int sc_main(int argc, char **argv) {
+
+  /*
+  sc_set_default_time_unit(1, SC_SEC);
+  sc_set_time_resolution(1, SC_MS);
+  */
+  assert(argc == 2);
+  sc_time sim_time(atof(argv[1]), SC_SEC);
+
+  // N<orth>G<enerator>_Sig<nal>
+  sc_signal<bool> NG_sig, SG_sig, EG_sig, WG_sig;
+  // N<orth>T<raffic>L<light>_Sig<nal>
+  sc_signal<bool> NTL_sig, STL_sig, ETL_sig, WTL_sig;
+  // N<orth>S<ensor>_Sig<nal>
+  sc_signal<bool> NS_sig, SS_sig, ES_sig, WS_sig;
+
+  Generator gen_north("North_traffic_generator");
+  gen_north(NG_sig);
+  Generator gen_south("South_traffic_generator");
+  gen_south(SG_sig);
+  Generator gen_west("West__traffic_generator");
+  gen_west(WG_sig);
+  Generator gen_east("East__traffic_generator");
+  gen_east(EG_sig);
+
+  Sensor sensor_north("North_traffic_Sensor");
+  sensor_north(NG_sig, NTL_sig, NS_sig);
+  Sensor sensor_south("South_traffic_Sensor");
+  sensor_south(SG_sig, STL_sig, SS_sig);
+  Sensor sensor_west("West__traffic_Sensor");
+  sensor_west(WG_sig, WTL_sig, WS_sig);
+  Sensor sensor_east("East__traffic_Sensor");
+  sensor_east(EG_sig, ETL_sig, ES_sig);
+
+  Controller traffic_light("Traffic light");
+  traffic_light(NS_sig, SS_sig, ES_sig, WS_sig, NTL_sig, STL_sig, ETL_sig, WTL_sig);
+
+  sc_start(sim_time);
+  return 0;
+}
